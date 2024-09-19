e37702442a9aa27d662c4816b8c8e831cb00ede9
JoSujin-cando <81570268+JoSujin-cando@users.noreply.github.com>
2024-09-19 22:51:27 +0900

    DB data to RecyclerView, InfoPage
    DB에 있는 데이터를 리사이클러뷰와 리사이클러뷰를 클릭했을 때 넘어가는 PerformList2 페이지에 전달해 보여주는 코드

--- app/src/main/java/com/example/gradfront/MainAdapter.kt
+++ app/src/main/java/com/example/gradfront/MainAdapter.kt
@@ -3,10 +3,12 @@ package com.example.gradfront
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.example.gradfront.data.LivePerformanceResponse
 import com.example.gradfront.databinding.ItemRecyclerViewBinding
 
 // ViewHolder와 Adapter 정의: item_recycler_view.xml에 있는 뷰를 바인딩하고 각 아이템에 필요한 정보도
-class MainAdapter(private val dataList: List<ItemData>, private val MainClick: (ItemData) -> Unit) :
+class MainAdapter(private val dataList: List<LivePerformanceResponse>, private val MainClick: (LivePerformanceResponse) -> Unit) :
     RecyclerView.Adapter<MainAdapter.MainViewHolder>() {
 
     class MainViewHolder(val binding: ItemRecyclerViewBinding) : RecyclerView.ViewHolder(binding.root)  //뷰홀더
@@ -18,9 +20,9 @@ class MainAdapter(private val dataList: List<ItemData>, private val MainClick: (
 
     override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
         val item = dataList[position]
-        holder.binding.textView20.text = item.title
-        holder.binding.textView28.text = item.subtitle
-        holder.binding.imageView4.setImageResource(item.imageResId)
+        holder.binding.perfTitle.text = item.title
+        holder.binding.bandLineUp.text = item.bandLineup
+        holder.binding.perfImg.load(item.image)
 
         // 아이템 클릭 리스너 설정: RecyclerView 아이템 클릭 이벤트 처리
         holder.itemView.setOnClickListener {
--- app/src/main/java/com/example/gradfront/PerformList2.kt
+++ app/src/main/java/com/example/gradfront/PerformList2.kt
@@ -3,23 +3,40 @@ package com.example.gradfront
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import androidx.lifecycle.ViewModelProvider
 import com.example.gradfront.databinding.ActivityPerformList2Binding
 
 class PerformList2 : AppCompatActivity() {
+    private lateinit var binding: ActivityPerformList2Binding
+    private lateinit var viewModel: MainViewModel
     var count = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivityPerformList2Binding.inflate(layoutInflater)
+        binding = ActivityPerformList2Binding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // **MainViewModel 초기화**
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
         // Intent로부터 데이터 받기
         val title = intent.getStringExtra("title")
-        val subtitle = intent.getStringExtra("subtitle")
-        val imageResId = intent.getIntExtra("imageResId", R.drawable.ic_launcher_background)
+        val date = intent.getStringExtra("date")
+        val club_id = intent.getStringExtra("club_id")
+        val genre = intent.getStringExtra("genre")
+        val advanceprice = intent.getStringExtra("advancePrice")
+        val notice = intent.getStringExtra("notice")
+        val timetable = intent.getStringExtra("timetable")
+        val imageResId = intent.getIntExtra("imageUrl", R.drawable.ic_launcher_background)
 
         // 받은 데이터를 UI에 표시
         binding.PerfTitle.text = "공연명: $title"
-        binding.PerfPlace.text = "공연일시: $subtitle"
+        binding.PerfDate.text = "공연일시: $date"
+        binding.PerfPlace.text = "공연장소: $club_id"
+        binding.genre.text = "장르: $genre"
+        binding.price.text = "예매가: $advanceprice"
+        binding.notice.text = "$notice"
+        binding.TimeTable.text = "$timetable"
         binding.imageView2.setImageResource(imageResId)
 
         //인원 수 버튼 클릭 시
